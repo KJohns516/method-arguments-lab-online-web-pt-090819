1ba5fb0886a36d53bda08574650d150e7a66a455
Kristan Johns <kristandjohns@gmail.com>
2019-09-10 18:55:27 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,2 +1,6 @@
 def introduction 
-  
+  expect {introduction("Christopher")}. to 
+  output("Hi, my name is Christopher please to meet you.\n").to_stdout
+          expected block to output
+          "Hi, my name is Christopher please to meet you.\n" to 
+          stdout, but output nothing
